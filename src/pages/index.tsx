a8a44da06c0d09b783558d1d628565a2f831d998
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-04-29 16:17:01 -0400

    Challenge Boxx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
+import Head from 'next/head';
+
 import { CompleteChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css'
-
-import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
